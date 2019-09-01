f80d4b2b4d4eb5c9ac0d93e2a3b24b161b5a44ec
Omar Diaz <omarjdiazv@gmail.com>
2019-09-01 16:21:09 -0300

    Created Carrousel's Categories

---
+++ src/resources/db.js
@@ -0,0 +1,54 @@
+const db = {
+  home: {
+    hero: {
+      image: 'https://e00-telva.uecdn.es/assets/multimedia/imagenes/2019/04/12/15550218021445.jpg',
+      title: 'New Arrivals',
+      categorie: '',
+      description: 'Check out our fresh new stuff from Fiori, store!',
+    },
+  },
+  products: [
+    {
+      id: 1,
+      name: 'Zapato azul',
+      image: 'url',
+      description: 'it is a nice shoes',
+      price: 200.22,
+      categories: [],
+    },
+  ],
+  categories: [
+    {
+      name: 'Woman',
+      image: 'https://dtw9wq3axq89t.cloudfront.net/thumbs/1200x900c/articles/poster/pinterest-1336x567.jpg',
+      id: 1,
+      subcategories: [],
+    },
+    {
+      name: 'Man',
+      image: 'https://image.dhgate.com/0x0/f2/albu/g7/M00/FE/39/rBVaSVravQCAZXHGAAU1arO-6u4521.jpg',
+      id: 2,
+      subcategories: [],
+    },
+    {
+      name: 'Children',
+      image: 'https://www.minimoda.es/wp-content/uploads/2017/05/carolina-herrera-moda-infantil-primavera-3.jpg',
+      id: 3,
+      subcategories: [],
+    },
+    {
+      name: 'Children',
+      image: 'https://www.minimoda.es/wp-content/uploads/2017/05/carolina-herrera-moda-infantil-primavera-3.jpg',
+      id: 3,
+      subcategories: [],
+    },
+  ],
+  wishlist: [],
+  userdetails: {
+    username: 'David Figueroa',
+    info: 'developer',
+    avatar: 'url',
+  },
+};
+
+export default db;
