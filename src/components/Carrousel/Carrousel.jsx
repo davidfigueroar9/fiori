a8c187c879a583a4245b178094bb8ad3a6a3073f
Omar Díaz <45800425+omarjdv@users.noreply.github.com>
2019-09-02 12:22:05 -0300

    Created Carrousel's Categories (#3)

---
+++ src/components/Carrousel/Carrousel.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Icon from 'components/Icon';
+
+import './styles.css';
+
+const Carrousel = ({ categories, title }) => (
+  <div className="Carrousel">
+    <div className="Carrousel-header">
+      <div className="Carrousel-header-title">{title}</div>
+      <div className="Carrousel-header-show">
+        <div className="">View All</div>
+      </div>
+    </div>
+    <div className="Carrousel-categories">
+      {categories.map(c => (
+        <div className="Carrousel-item" style={{ backgroundImage: `url(${c.image})` }}>
+          {c.name}
+          <Icon name="next-1" />
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+Carrousel.propTypes = {
+  title: PropTypes.string.isRequired,
+  categories: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+
+export default Carrousel;
